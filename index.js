0fcf77d91d04559606b79dc4f001d3b35f968949
Shalini Anand <42630854+liniii31@users.noreply.github.com>
2022-10-16 16:29:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let readAll = document.getElementsByClassName('mark-as-read')[0];
+let notRead = document.getElementsByClassName('not-read');
+let count = parseInt(document.getElementsByClassName('notification-count')[0].innerHTML);
+readAll.addEventListener('click',function(){
+    for(ele of notRead){
+        ele.style.backgroundColor = 'white';
+        count--;        
+    } 
+    if(count<0){
+        count=0;
+    } 
+    document.getElementsByClassName('notification-count')[0].innerHTML = count;  
+})
+for(ele of notRead){
+    ele.addEventListener('click',function(){
+        this.style.backgroundColor = 'white';
+        count--;
+        document.getElementsByClassName('notification-count')[0].innerHTML = count; 
+    })
+}
